d065ac040fdc419b832f68eb2c8cdef3e8bfabd9
yazawayuma1203 <110238678+yazawayuma1203@users.noreply.github.com>
2022-07-31 23:34:42 +0900

    未完了を5個までの機能実装 未完了のTODOが5個以上ある場合、メッセージを表示し、追加できない

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,13 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>
+          登録できるTODOは5個までです。消化してください。
+        </p>
+      )}
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
